27c2db38f781c3204ad4c8feacb366ed8e82a4b2
cj-anderson <andersoncaleb546@gmail.com>
2025-03-03 15:48:26 -0500

    Update documentation in Piecewise.java

--- java/src/main/java/Piecewise.java
+++ java/src/main/java/Piecewise.java
@@ -5,10 +5,17 @@ public class Piecewise {
     int step;
     String filename;
 
+    //Generate filename.
     private String genFileName(int core){
         return filename.replace(".txt", "-core-" + String.format("%02d",core) + ".txt");
     }
 
+     /**
+    * A function used to perform piecewise linear interpolation base on
+    * two points, given by x1, x2, y1, and y2.
+    *
+    * @param x1,x2,y1,y2 used to perform linear interpolation
+    */
     private double[] PiecewiseInterpolation(int x1, int x2, double y1, double y2){
         double numerator = y2 - y1;
         double denominator = x2 - x1;
@@ -19,12 +26,24 @@ public class Piecewise {
         return new double[]{m, b};
     }
 
+     /**
+     * The main function used to use the functions of the Piecewise class.
+     *
+     * All data members are stored in a class object of this file.
+     * @param readings An array of readings passed in from the main class.
+     * @param step the length, in seconds, of time between readings.
+     * @param filename the name of the input file, used to generate the output file names.
+     */
     public void linearInterpolation (){
+        //Loop through each core.
         for (int i = 0; i < readings.length; i++){
             String coreFileName = genFileName(i);
             
             try{
+                //Create a new output file.
                 FileWriter out =  new FileWriter(new File(coreFileName));
+
+                //Perform the actual Piecewise Linear Interpolation.
                 for(int j = 0; j < readings[i].length-1; j++){
                     double[] data = PiecewiseInterpolation(
                         j * step, (j+1) * step, readings[i][j], readings[i][j+1]); 
@@ -34,11 +53,10 @@ public class Piecewise {
                 }
                 out.close();
             }catch(IOException ex){
+                //Handle IO Exceptions.
                 System.err.println("Error creating output file for core " + i
                 + ex.getMessage());
             }
-
-            
         }
     }
 }
